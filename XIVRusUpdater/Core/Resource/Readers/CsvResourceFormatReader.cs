e0fb79122f2801cdd5405dd9f6f596d1d9a349e6
hOst1k <gubanoff1337@gmail.com>
2026-08-23 20:49:27 +0300

    Implement CSV reader with Sep

--- XIVRusUpdater/Core/Resource/Readers/CsvResourceFormatReader.cs
+++ XIVRusUpdater/Core/Resource/Readers/CsvResourceFormatReader.cs
@@ -1,3 +1,4 @@
+using nietras.SeparatedValues;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,14 +7,33 @@ using XIVRusUpdater.Utils;
 
 namespace XIVRusUpdater.Core.Resource.Readers;
 
+//Relative easy task, no overload: https://www.joelverhagen.com/blog/2020/12/fastest-net-csv-parsers
+//Fastest lib - Sep. 
 internal sealed class CsvResourceFormatReader : IResourceFormatReader
 {
     public Dictionary<uint, List<ByteArrayWrapper>> Read(Stream stream)
     {
-        using var reader = new StreamReader(stream);
         var rows = new Dictionary<uint, List<ByteArrayWrapper>>();
 
-        // TODO: Парсинг CSV
+        using var reader = Sep.Reader(o => o with { HasHeader = false }).From(stream);
+
+        foreach (var row in reader)
+        {
+            var rowId = row[0].Parse<uint>();
+
+            var columns = new List<ByteArrayWrapper>(row.ColCount - 1);
+
+            for (var i = 1; i < row.ColCount; i++)
+            {
+                var span = row[i].Span;
+                var byteCount = Encoding.UTF8.GetByteCount(span);
+                var bytes = new byte[byteCount];
+                Encoding.UTF8.GetBytes(span, bytes);
+                columns.Add(new ByteArrayWrapper(bytes));
+            }
+
+            rows[rowId] = columns;
+        }
 
         return rows;
     }
