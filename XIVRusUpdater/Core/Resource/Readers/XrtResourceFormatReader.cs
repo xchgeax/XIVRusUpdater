f5d4f4e91e477de98569034cbbd03b24c80c7464
hOst1k <gubanoff1337@gmail.com>
2026-08-24 17:46:04 +0300

    Implement XRT reading logic

--- XIVRusUpdater/Core/Resource/Readers/XrtResourceFormatReader.cs
+++ XIVRusUpdater/Core/Resource/Readers/XrtResourceFormatReader.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using XIVRusUpdater.Utils;
+using XIVRusUpdater.Utils.Extentions;
 
 namespace XIVRusUpdater.Core.Resource.Readers;
 
 internal sealed class XrtResourceFormatReader : IResourceFormatReader
 {
+    private static byte[] Magic = "XRT\0"u8.ToArray();
+
     public Dictionary<uint, List<ByteArrayWrapper>> Read(Stream stream)
     {
         using var reader = new BinaryReader(stream);
+
+        byte[] magic = reader.ReadBytes(Magic.Length);
+
+        if (!magic.AsSpan().SequenceEqual(Magic))
+            throw new InvalidDataException("Invalid XRT format.");
+
+        ushort version = reader.ReadUInt16();
+
+        if (version != 1)
+            throw new InvalidDataException($"Unsupported XRT version: {version}");
+
+        ushort stringColumnCount = reader.ReadUInt16();
+
         var rows = new Dictionary<uint, List<ByteArrayWrapper>>();
 
-        // TODO: Реализовать формат .xrt
+
+        while (stream.Position < stream.Length)
+        {
+            long remain = stream.Length - stream.Position;
+
+            if (remain < sizeof(uint))
+                throw new InvalidDataException("Incomplete XRT row header.");
+
+            uint rowId = reader.ReadUInt32();
+
+            var columns = new List<ByteArrayWrapper>(stringColumnCount);
+
+            for (int i = 0; i < stringColumnCount; i++)
+            {
+                byte[] value = reader.ReadStringNullterminated();
+                columns.Add(new ByteArrayWrapper(value));
+            }
+
+            rows.Add(rowId, columns);
+        }
 
         return rows;
     }
